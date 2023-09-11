375d44911967a5f866345a8f861a6bd9584ae44d
João Vitor <joao-j-v@hotmail.com>
2023-09-11 01:16:52 -0300

    primeiro commit

---
+++ js/aula-2/script.js
@@ -0,0 +1,32 @@
+// tipos primitivos
+
+// boolean
+
+var vOuF = false;
+console.log(typeof(vOuF));
+
+// number
+
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+// string
+
+var nome = 'diana';
+console.log(typeof(nome));
+
+// function
+
+var funcao = function() {}
+console.log(typeof(funcao));
+
+// como declarar
+
+/*
+var variavel = 'diana';
+variavel = 'martine';
+console.log(variavel);
+*/
+
+let variavel2 = 'diana';
+console.log(variavel2);
