c33058cea21e33c602ece1a6e60c99e071021295
João Vitor <joao-j-v@hotmail.com>
2024-01-21 07:08:48 -0300

    pegando props e criando o estilo do card direto no component

--- angular/01-web-components/src/Components/CardNews.js
+++ angular/01-web-components/src/Components/CardNews.js
@@ -16,13 +16,15 @@ class CardNews extends HTMLElement {
         cardLeft.setAttribute("class", "card_left");
 
         const author = document.createElement("span");
-        author.textContent = "By " + (this.getAttribute("author") || "Anonymous")
+        author.textContent =
+            "By " + (this.getAttribute("author") || "Anonymous");
 
         const linkTitle = document.createElement("a");
         linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url");
 
         const newsContent = document.createElement("p");
-        newsContent.textContent = this.getAttribute("content")
+        newsContent.textContent = this.getAttribute("content");
 
         cardLeft.appendChild(author);
         cardLeft.appendChild(linkTitle);
@@ -30,7 +32,10 @@ class CardNews extends HTMLElement {
 
         const cardRight = document.createElement("div");
         cardRight.setAttribute("class", "card_right");
+
         const newsImage = document.createElement("img");
+        newsImage.src = this.getAttribute("photo") || "assets/foto-default.jpg";
+        newsImage.alt = "Foto da notícia";
         cardRight.appendChild(newsImage);
 
         componentRoot.appendChild(cardLeft);
@@ -39,7 +44,45 @@ class CardNews extends HTMLElement {
         return componentRoot;
     }
 
-    styles() {}
+    styles() {
+        const style = document.createElement("style");
+        style.textContent = `
+            .card {
+                width: 80%;
+                box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
+                -webkit-box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
+                -moz-box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
+            }
+
+            .card_left {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                padding-left: 10px;
+            }
+
+            .card_left > span {
+                font-weight: 400;
+            }
+
+            .card_left > a {
+                margin-top: 15px;
+                font-size: 25px;
+                color: black;
+                text-decoration: none;
+                font-weight: bold;
+            }
+
+            .card_left > p {
+                color: gray;
+            }
+            `;
+        
+        return style;
+        }
 }
 
 customElements.define("card-news", CardNews);
